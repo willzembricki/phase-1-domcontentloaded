3f60b45a8960617ee5a4b3a477f2cb4550658a46
William Zembricki <wzembricki@gmail.com>
2021-03-17 20:24:23 -0400

    commit tree 2130aedbfed2203057b54b1d28781e0165fabf15

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let titleNode = document.getElementById("text")
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    titleNode.textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
+    
+  }); 
